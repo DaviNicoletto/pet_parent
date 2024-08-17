46d422546fd76112013a99a3040bffa4ea893279
Davi Nicoletto <davinicolettofs7@gmail.com>
2024-08-17 16:52:48 -0300

    Feat [app]: Added Firestore provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:pet_parent/src/services/auth_service.dart';
+import 'package:pet_parent/src/services/firestore_db.dart';
 import 'src/app.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-
-WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(MultiProvider(
-    providers: [ChangeNotifierProvider(create: (context) => DBAuth()),],
+    providers: [
+      ChangeNotifierProvider(create: (context) => DBAuth()),
+      ChangeNotifierProvider(create: (context) => CloudDatabase()),
+    ],
     child: const MyApp(),
   ));
 }
